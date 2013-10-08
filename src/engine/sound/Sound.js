b79c7a8cebd9480dc4b43ce188107257316a042a
Janosch Woschitz <janosch.woschitz@gmail.com>
2013-10-09 00:25:23 +0200

    code formatting

--- src/engine/sound/Sound.js
+++ src/engine/sound/Sound.js
@@ -1,21 +1,24 @@
-define(["engine/UA"], function(UA) {
+define(["engine/UA"], function (UA) {
 
-	var Sound = function(url) {
-      var audObj = new Audio('');
-      
-      // The actual audio object won't get a src until load is called
-      audObj.url = url;
-      
-      return audObj;
+    var Sound = function (url) {
+        var audObj = new Audio('');
+
+        // The actual audio object won't get a src until load is called
+        audObj.url = url;
+
+        return audObj;
     };
-	
-	var MockSound = function() {
-		return {
-			play: function() {},
-			pause: function() {},
-			load: function() {}
-		}
-	};
-	
-	return (UA.safari) ? MockSound : Sound;
+
+    var MockSound = function () {
+        return {
+            play: function () {
+            },
+            pause: function () {
+            },
+            load: function () {
+            }
+        }
+    };
+
+    return (UA.safari) ? MockSound : Sound;
 });
